5c80395aa612803440718cd0a09393274cc876cd
Ben H <bhaszard@bauer-media.com.au>
2015-08-19 16:58:26 +1000

    .

--- package-d.cmd
+++ package-d.cmd
@@ -1,2 +1,2 @@
-..\neo4jclient\.nuget\nuget.exe pack ..\neo4jclient\neo4jclient\Neo4jClient.nuspec -OutputDirectory . -Version 1.1.1.0-nm -Properties Configuration=debug
+..\neo4jclient\.nuget\nuget.exe pack ..\neo4jclient\neo4jclient\Neo4jClient.nuspec -OutputDirectory . -Version 1.1.1.1-nm -Properties Configuration=debug
 
